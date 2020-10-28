956cf351a80d79d0107163c6afb4bac29474650d
Mariia Trusova <tmv_2008@ukr.net>
2020-10-28 20:07:59 +0200

    Initial commit

---
+++ js_practica1.js
@@ -0,0 +1,105 @@
+let hello = 'Hello';
+
+alert(hello);
+console.log(hello);
+document.write(hello);
+
+let owu = 'Owu';
+
+alert(owu);
+console.log(owu);
+document.write(owu);
+
+let com = 'com';
+
+alert(com);
+console.log(com);
+document.write(com);
+
+let ua = 'ua';
+
+alert(ua);
+console.log(ua);
+document.write(ua);
+
+let one = 1;
+
+alert(one);
+console.log(one);
+document.write(one);
+
+let numeral2 = 10;
+
+alert(numeral2);
+console.log(numeral2);
+document.write(numeral2);
+
+let numeral3 = -999;
+
+alert(numeral3);
+console.log(numeral3);
+document.write(numeral3);
+
+let numeral4 = 123;
+
+alert(numeral4);
+console.log(numeral4);
+document.write(numeral4);
+
+let numeral5 = 3.14;
+
+alert(numeral5);
+console.log(numeral5);
+document.write(numeral5);
+
+let numeral6 = 2.7;
+
+alert(numeral6);
+console.log(numeral6);
+document.write(numeral6);
+
+let numeral7 = 16;
+
+alert(numeral7);
+console.log(numeral7);
+document.write(numeral7);
+
+let answer = true;
+
+alert(answer);
+console.log(answer);
+document.write(answer);
+
+let answer2 = false;
+
+alert(answer2);
+console.log(answer2);
+document.write(answer2);
+
+///
+
+hello = 'Hi !';
+owu = 'okten';
+com = 'web';
+ua = 'Pupkin';
+one = 'Vasya';
+numeral2 = '7777777';
+numeral3 = '555';
+numeral4 = '0';
+numeral5 = '888';
+numeral6 = 'good';
+numeral7 = 'day';
+answer = '!!!!!';
+answer2 = ')))))';
+
+///
+
+let variable1 = 777;
+let variable2 = 55;
+let variable3 = -111;
+let variable4 = '777';
+let variable5 = '44';
+let variable6 = '000';
+
+
+
